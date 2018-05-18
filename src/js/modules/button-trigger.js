51bb636ff15389fdef3328e7569e1f397432a5ac
quondominium <quondominium@gmail.com>
2018-05-18 16:59:38 +0300

    header block
    added media queries

--- src/js/modules/button-trigger.js
+++ src/js/modules/button-trigger.js
@@ -1,8 +1,3 @@
-// You can write a call and import your functions in this file.
-//
-// This file will be compiled into app.js and will not be minified.
-// Feel free with using ES6 here.
-
 			 function listen() {
 			    var HAMBURGER_ACTIVE = 'is-active';
 			    var NAVIGATION_OPENED = 'is-navigation-opened';
